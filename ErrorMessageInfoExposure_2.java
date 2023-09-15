a0687e7cbabec56c7ae30c48e255fe7e0fdef1c2
jakkli-mend <117362921+jakkli-mend@users.noreply.github.com>
2023-09-15 16:53:25 +0200

    Add files via upload

---
+++ ErrorMessageInfoExposure_2.java
@@ -0,0 +1,49 @@
+@RestController
+public class SqlInjectionLesson6b extends AssignmentEndpoint {
+
+  private final LessonDataSource dataSource;
+
+  public SqlInjectionLesson6b(LessonDataSource dataSource) {
+    this.dataSource = dataSource;
+  }  
+  
+  
+  @PostMapping("/SqlInjectionAdvanced/attack6b")
+  @ResponseBody
+  public AttackResult completed(@RequestParam String userid_6b) throws IOException {
+    if (userid_6b.equals(getPassword())) {
+      return success(this).build();
+    } else {
+      return failed(this).build();
+    }
+  }
+    
+  protected String getPassword() {
+    String password = "dave";
+    try (Connection connection = dataSource.getConnection()) {
+      String query = "SELECT password FROM user_system_data WHERE user_name = 'dave'";
+      try {
+        Statement statement =
+            connection.createStatement(
+                ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet results = statement.executeQuery(query);
+
+        if (results != null && results.first()) {
+          password = results.getString("password");
+        }
+      } catch (SQLException sqle) {
+        sqle.printStackTrace();
+        // do nothing
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      // do nothing2
+      
+    }
+    return (password);
+  }
+  
+
+  private String tmp = "1";
+  private String tmp2 = "2";
+}
